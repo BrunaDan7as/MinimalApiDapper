0f43d8d0636cd531218bff40ce46a667d0d6530b
Bruna Dantas <b.dantassoares@gmail.com>
2024-04-16 22:14:24 -0300

    Store procedure do Insert feito.

---
+++ ShopDB/dbo/StoreProcedures/spCustomer_Insert.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spCustomer_Insert]
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50)
+AS
+	INSERT INTO dbo.Customer (FirstName, LastName)
+	VALUES (@FirstName, @LastName)
+RETURN 0
